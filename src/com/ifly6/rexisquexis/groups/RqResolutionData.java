12e761fb32d68474ac48dd73c3e2c6f6fec5cf28
ifly6 <ifly6@me.com>
2020-06-26 13:40:07 -0400

    ran reformat

--- src/com/ifly6/rexisquexis/groups/RqResolutionData.java
+++ src/com/ifly6/rexisquexis/groups/RqResolutionData.java
@@ -5,59 +5,59 @@ package com.ifly6.rexisquexis.groups;
  */
 public class RqResolutionData {
 
-	private String resolutionName;
-	private int num;
-	private String category;
-	private String strength;
-	private boolean repealed;
-	private String author;
+    private String resolutionName;
+    private int num;
+    private String category;
+    private String strength;
+    private boolean repealed;
+    private String author;
 
-	private int postNum;
+    private int postNum;
 
-	public RqResolutionData(String resolutionName, int resolutionNum, String category, String strength, int postNum,
+    public RqResolutionData(String resolutionName, int resolutionNum, String category, String strength, int postNum,
                             boolean repealed, String author) {
-		this.resolutionName = resolutionName;
-		this.num = resolutionNum;
-		this.category = category;
-		this.strength = strength;
-		this.postNum = postNum;
-		this.repealed = repealed;
-		this.author = author;
-	}
+        this.resolutionName = resolutionName;
+        this.num = resolutionNum;
+        this.category = category;
+        this.strength = strength;
+        this.postNum = postNum;
+        this.repealed = repealed;
+        this.author = author;
+    }
 
-	public String name() {
-		return resolutionName;
-	}
+    public String name() {
+        return resolutionName;
+    }
 
-	public int num() {
-		return num;
-	}
+    public int num() {
+        return num;
+    }
 
-	public String strength() {
-		return strength;
-	}
+    public String strength() {
+        return strength;
+    }
 
-	public String category() {
-		return category;
-	}
+    public String category() {
+        return category;
+    }
 
-	public int postNum() {
-		return postNum;
-	}
+    public int postNum() {
+        return postNum;
+    }
 
-	public boolean isRepealed() {
-		return repealed;
-	}
+    public boolean isRepealed() {
+        return repealed;
+    }
 
-	public void setRepealed(boolean repealed) {
-		this.repealed = repealed;
-	}
+    public void setRepealed(boolean repealed) {
+        this.repealed = repealed;
+    }
 
-	public String getAuthor() {
-		return author;
-	}
+    public String getAuthor() {
+        return author;
+    }
 
-	public void setAuthor(String author) {
-		this.author = author;
-	}
+    public void setAuthor(String author) {
+        this.author = author;
+    }
 }
--- src/com/ifly6/rexisquexis/package-info.java
+++ src/com/ifly6/rexisquexis/package-info.java
@@ -1,12 +1,10 @@
 /**
  * Classes in this package depend on a frankly ridiculous number of dependencies. Please do not actually use them in
- * deployment. Due to arbitrary and unpredictable changes in the NationStates API, I wholly recommend that you use
- * this entirely in an IDE like IntelliJ and deploy, debug, and test from there.
- *
- * To make this work in IntelliJ you will also need to download the JavaTelegram library and add that as a dependency.
- * I should note that the JavaTelegram module's NS-API components are currently under active development, so they may
+ * deployment. Due to arbitrary and unpredictable changes in the NationStates API, I wholly recommend that you use this
+ * entirely in an IDE like IntelliJ and deploy, debug, and test from there.
+ * To make this work in IntelliJ you will also need to download the JavaTelegram library and add that as a dependency. I
+ * should note that the JavaTelegram module's NS-API components are currently under active development, so they may
  * change erratically as well.
- *
  * Frankly, I don't recommend you actually run the code here.
  */
 package com.ifly6.rexisquexis;
