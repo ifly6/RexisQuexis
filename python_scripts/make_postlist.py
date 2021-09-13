b48ca77f0add5e24b3044e3cbae6fb42ae6ab903
ifly6 <ifly6@me.com>
2021-09-13 17:01:06 -0400

    comment docs for make_postlist

--- python_scripts/make_postlist.py
+++ python_scripts/make_postlist.py
@@ -15,31 +15,45 @@ from bs4 import BeautifulSoup
 Post = namedtuple('Post', ['title', 'post', 'repealed', 'is_empty'])
 url = 'https://forum.nationstates.net/viewtopic.php?f=9&t=30&start={}'
 
+# create containers and helper functions for parsing
 seen_list = []
 titles_and_posts = []
 
 
 def is_repealed(i):
-    if '[REPEALED]' in post_title.text:
-        return True
-
     post_text = str(i.parent.parent)
-    if ('<del>' in post_text and '</del>' in post_text
-            and ('struck out by' in post_text.lower() or 'repealed by' in post_text.lower())):
-        return True
+
+    # this isn't always marked properly... but it should be dispositive if marked
+    if '[REPEALED]' in post_title.text:
+        if '<del>' in post_text:
+            return True
+        else:
+            # shouldn't ever happen, check for consistency!
+            raise RuntimeError(f'post {i} marked as repealed but does not have strike-throughs!')
+
+    # strikethrough is present only in repealed resolutions
+    if '<del>' in post_text and '</del>' in post_text:
+        if 'struck out by' in post_text.lower() or 'repealed by' in post_text.lower():
+            return True
+
+        else:
+            # this also shouldn't ever happen, do manual check for consistency...
+            raise RuntimeError(f'post {i} contains del tags but no "repealed by" tags!')
 
     return False
 
 
 def is_empty(i):
     post_text = post_title.parent.parent.select('div.content')[0].text.strip()
-    if (post_text == '.'
-            or len(post_text) == 0):
+
+    # normal posts should never start with a full stop
+    if (post_text.startswith('.') or len(post_text) == 0):
         return True
 
     return False
 
 
+# parse the raw forum data
 no_more = False
 for i in range(0, 40):
     if no_more is True:
@@ -69,14 +83,17 @@ for i in range(0, 40):
 
     time.sleep(5)
 
+# create data frame for tabular repr
 posts = pd.DataFrame(titles_and_posts)
 
-resolutions = posts.loc[2:].copy()
+resolutions = posts.loc[2:].copy()  # manual exclude rexis intro and contents posts
 resolutions['code'] = resolutions.apply(
-    lambda r: '[post={}]{}[/post]'.format(r['post'], r['title']), axis=1)
+    lambda r: '[post={}]{}[/post]'.format(r['post'], r['title']), axis=1)  # make post bbcode
 
+# shift using original index to pages
 resolutions['page'] = (resolutions.index // 25) + 1
 
+# create page boundaries then for each page, generate and append links
 lines = ['[list=1][b][color=darkblue]Page 1[/color][/b]', '']
 for g, df in resolutions.groupby('page'):
     if g != 1:
@@ -90,5 +107,8 @@ for g, df in resolutions.groupby('page'):
             lines.append('[*]' + r['code'] +
                          (' [b][REPEALED][/b]' if r['repealed'] else ''))
 
+# save to var and print
 contents = '\n'.join(lines) + '[/list]'
 print(contents)
+
+# Nb! if using var explorer in spyder, open the string in that and copy it all for pasting!
